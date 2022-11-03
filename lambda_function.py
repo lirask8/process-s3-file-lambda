3b241093e94e2a6fc349a32eac740dfc7278b794
Oscar Lira <olira@Oscars-MacBook-Pro.local>
2022-11-03 15:12:50 -0600

    Feature: Add email SES function

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,16 @@
 import boto3
+from botocore.exceptions import ClientError
 
 from process_file import ProcessFile
 
 s3_client = boto3.client("s3")
 S3_BUCKET_NAME = 'public-images-ols3'
+SENDER = "Sender Name <lira.sk8.182@gmail.com>"
+AWS_REGION = "us-east-2"
+SUBJECT = "Stori transactions summary"
+BODY_TEXT = ("Summary transactions")
+RECIPIENT = "lira.sk8.182@gmail.com"
+CHARSET = "UTF-8"
 
 
 def lambda_handler(event, context):
@@ -16,12 +23,55 @@ def lambda_handler(event, context):
     summary = file_processor.get_summary_data()
 
     print(summary)
-    # send email SES
+
+    send_email_ses(summary)
 
     return {
         'statusCode': 200,
         'body': summary,
     }
+
+
+def send_email_ses(summary: dict):
+    BODY_HTML = f"<html><head></head><body><h1>Summary Transactions</h1>" \
+                f"<p>Balance: {summary['balance']}</p>" \
+                f"<p>Debit Avg: {summary['debit_avg']}</p>" \
+                f"<p>Credit Avg: {summary['credit_avg']}</p>" \
+                f"</body></html>"
+
+    client = boto3.client('ses', region_name=AWS_REGION)
+
+    try:
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    RECIPIENT,
+                ],
+            },
+            Message={
+                'Body': {
+                    'Html': {
+                        'Charset': CHARSET,
+                        'Data': BODY_HTML,
+                    },
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': BODY_TEXT,
+                    },
+                },
+                'Subject': {
+                    'Charset': CHARSET,
+                    'Data': SUBJECT,
+                },
+            },
+            Source=SENDER,
+        )
+    except ClientError as e:
+        print("Error>>>>>>>>>>>>>>>")
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+        print(response['MessageId'])
 
 # RUN FUNCTION
 lambda_handler("event", {})
